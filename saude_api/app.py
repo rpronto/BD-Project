b23e14c0839a61f0a7ae4945647e2ff826711d2e
Leticia <leticia.vong@tecnico.ulisboa.pt>
2024-05-27 14:12:13 +0100

    app registar consulta :)

--- saude_api/app.py
+++ saude_api/app.py
@@ -7,6 +7,9 @@ from logging.config import dictConfig
 import psycopg
 from flask import Flask, jsonify, request
 from psycopg.rows import namedtuple_row
+from datetime import datetime
+import random
+import string
 
 # Use the DATABASE_URL environment variable if it exists, otherwise use the default.
 # Use the format postgres://username:password@hostname/database_name to connect to the database.
@@ -78,7 +81,6 @@ def list_especialidades(clinica):
 
 
 
-
 @app.route("/c/<clinica>/<especialidade>/", methods=("GET",))
 def list_medicos(clinica, especialidade):
 
@@ -113,63 +115,151 @@ def list_medicos(clinica, especialidade):
 
     return jsonify(result)
 
+    
+def is_valid_hour(hora):
+    try:
+        # Converte a string de hora para um objeto datetime
+        hora_obj = datetime.strptime(hora, "%H:%M:%S")
+        return True
+    except ValueError:
+        # Se a conversão falhar, a hora é inválida
+        return False
+
+
+def is_valid_date(data):
+    try:
+        data_obj = datetime.strptime(data, "%Y-%m-%d")
+        if data_obj.year in [2023, 2024]:
+            return True
+        else:
+            return False
+    except ValueError:
+        return False
 
 
 
+def generate_codigo_sns():
+    ''' Generates a unique codigo_sns for consulta. '''    
+    with psycopg.connect(conninfo=DATABASE_URL) as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            while True:
+                codigo_sns = ''.join(random.choices(string.digits, k=12))
+                cur.execute(
+                    """
+                    SELECT 1
+                    FROM consulta c
+                    WHERE c.codigo_sns = %(codigo)s;
+                    """,
+                    {"codigo": codigo_sns}
+                )
+                if cur.fetchone() is None:
+                    break
+        log.debug(f"Found {cur.rowcount} rows.")
 
-@app.route('/a/<clinica>/registar/', methods=['POST'])
+    return codigo_sns
+                    
+
+def get_next_consulta_id():
+    with psycopg.connect(conninfo=DATABASE_URL) as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            max_id = cur.execute(
+                """
+                SELECT MAX(id) 
+                FROM consulta;
+                """,
+                (),
+            ).fetchone()
+            log.debug(f"Found {cur.rowcount} rows.")
+    for id in max_id:
+        return id + 1
+
+
+
+@app.route('/a/<clinica>/registar/', methods=("POST",))
 def register_consulta(clinica):
-    data = request.get_json()
-    paciente = data['paciente']
-    medico = data['medico']
-    data_consulta = data['data']
-    hora_consulta = data['hora']
+    ''' Registers new appointment. '''
 
-    conn = get_db_connection()
-    cur = conn.cursor()
-    try:
-        cur.execute('BEGIN')
-        cur.execute('''
-            INSERT INTO consulta (ssn, nif, nome, data, hora)
-            VALUES (%s, %s, %s, %s, %s)
-        ''', (paciente, medico, clinica, data_consulta, hora_consulta))
-        cur.execute('COMMIT')
-        response = {'status': 'success', 'message': 'Consulta registrada com sucesso.'}
-    except Exception as e:
-        cur.execute('ROLLBACK')
-        response = {'status': 'error', 'message': f'Erro ao registrar consulta: {str(e)}'}
-    finally:
-        cur.close()
-        conn.close()
-    
-    return jsonify(response)
+    paciente = request.json.get("paciente")
+    medico = request.json.get("medico")
+    data_consulta = request.json.get("data")
+    hora_consulta = request.json.get("hora")
 
-@app.route('/a/<clinica>/cancelar/', methods=['POST'])
-def cancel_consulta(clinica):
-    data = request.get_json()
-    paciente = data['paciente']
-    medico = data['medico']
-    data_consulta = data['data']
-    hora_consulta = data['hora']
+    error = None
+    if not paciente:
+         error = "Paciente is required."
+    elif not medico:
+         error = "Medico is required."
+    elif not data_consulta:
+        error = "Data de consulta is required."
+    elif not hora_consulta:
+        error = "Hora de consulta is required."
 
-    conn = get_db_connection()
-    cur = conn.cursor()
-    try:
-        cur.execute('BEGIN')
-        cur.execute('''
-            DELETE FROM consulta 
-            WHERE ssn = %s AND nif = %s AND nome = %s AND data = %s AND hora = %s
-        ''', (paciente, medico, clinica, data_consulta, hora_consulta))
-        cur.execute('COMMIT')
-        response = {'status': 'success', 'message': 'Consulta cancelada com sucesso.'}
-    except Exception as e:
-        cur.execute('ROLLBACK')
-        response = {'status': 'error', 'message': f'Erro ao cancelar consulta: {str(e)}'}
-    finally:
-        cur.close()
-        conn.close()
-    
-    return jsonify(response)
+    if (not is_valid_hour(hora_consulta)):
+        error = "Formato hora de consulta incorreto."
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    if (not is_valid_date(data_consulta)):
+        error = "Formato data de consulta incorreto."
+
+    if error is not None:
+        return error, 400
+    else:
+        id = get_next_consulta_id()
+        codigo_sns = generate_codigo_sns()
+
+        with psycopg.connect(conninfo=DATABASE_URL) as conn:
+            with conn.cursor(row_factory=namedtuple_row) as cur:
+                try:
+                    
+                    cur.execute(
+                        """
+                        INSERT INTO consulta (id, ssn, nif, nome, data, hora, codigo_sns)
+                        VALUES (%(id)s, %(paciente)s, %(medico)s, %(clinica)s, 
+                        %(data_consulta)s, %(hora_consulta)s, %(codigo_sns)s);
+                        """,
+                        {"id": id, 
+                        "paciente": paciente, 
+                        "medico": medico, 
+                        "clinica": clinica, 
+                        "data_consulta": data_consulta, 
+                        "hora_consulta": hora_consulta, 
+                        "codigo_sns": codigo_sns},
+                    )
+                    conn.commit()
+                    response = {'status': 'success', 'message': 'Consulta registrada com sucesso.'}
+                except Exception as e:
+                    cur.execute('ROLLBACK')
+                    response = {'status': 'error', 'message': f'Erro ao registrar consulta: {str(e)}'}
+        return jsonify(response)
+
+
+
+
+
+
+
+
+@app.route("/accounts/<account_number>/delete", methods=("POST",))
+def account_delete(account_number):
+    """Delete the account."""
+
+    with psycopg.connect(conninfo=DATABASE_URL) as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute(
+                """
+                DELETE FROM account
+                WHERE account_number = %(account_number)s;
+                """,
+                {"account_number": account_number},
+            )
+        conn.commit()
+    return "", 204
+
+
+@app.route("/ping", methods=("GET",))
+def ping():
+    log.debug("ping!")
+    return jsonify({"message": "pong!", "status": "success"})
+
+
+if __name__ == "__main__":
+    app.run()
